0d27495e41472fe5c278d800638510d6e548e3f5
Devair Luiz Moitim <dmoitim@gmail.com>
2018-08-24 06:27:55 -0300

    Ajustes de layout (scroll) e implementação da MainViewModel.

---
+++ lands/lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lands.Infrastructure
+{
+    using ViewModels;
+
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
