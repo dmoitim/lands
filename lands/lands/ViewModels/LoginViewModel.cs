3449bc355b28884dc55938d86c14e19f3e4ba1e1
Devair Luiz Moitim <dmoitim@gmail.com>
2018-08-25 16:36:15 -0300

    Implementação do binding usando INotifyPropertyChanged.

--- lands/lands/ViewModels/LoginViewModel.cs
+++ lands/lands/ViewModels/LoginViewModel.cs
@@ -1,23 +1,37 @@
 ﻿namespace lands.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
     using System.Windows.Input;
     using Xamarin.Forms;
 
-    class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+        private string senha;
+        private bool activity;
+        private bool habilitado;
+        #endregion
+
         #region Properties
         public string Email { get; set; }
-        public string Senha { get; set; }
-        public bool Activity { get; set; }
+        public string Senha {
+            get { return this.senha; }
+            set { SetValue(ref senha, value); }
+        }
+        public bool Activity {
+            get { return this.activity; }
+            set { SetValue(ref activity, value); }
+        }
         public bool Lembrar { get; set; }
+        public bool Habilitado {
+            get { return this.habilitado; }
+            set { SetValue(ref habilitado, value); }
+        }
         #endregion
 
         #region Commands
-        public ICommand LoginCommand {
+        public ICommand LoginCommand
+        {
             get
             {
                 return new RelayCommand(Login);
@@ -26,7 +40,8 @@
 
         public ICommand RegistrarCommand { get; set; }
 
-        private async void Login() {
+        private async void Login()
+        {
             if (string.IsNullOrEmpty(this.Email))
             {
                 await Application.Current.MainPage.DisplayAlert(
@@ -47,15 +62,26 @@
                 return;
             }
 
+            this.Activity = true;
+            this.Habilitado = false;
+
             if (this.Email != "dmoitim@gmail.com" || this.Senha != "1234")
             {
+                this.Activity = false;
+                this.Habilitado = true;
+
                 await Application.Current.MainPage.DisplayAlert(
                     "Erro",
                     "Usuário ou senha inváidos.",
                     "OK");
 
+                this.Senha = string.Empty;
+
                 return;
             }
+
+            this.Activity = false;
+            this.Habilitado = true;
 
             await Application.Current.MainPage.DisplayAlert(
                 "Sucesso",
@@ -68,6 +94,7 @@
         public LoginViewModel()
         {
             this.Lembrar = true;
+            this.Habilitado = true;
         }
         #endregion
     }
