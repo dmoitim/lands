f8c1dbc31f26d114a1e2d6061099feeeadc43a30
Devair Luiz Moitim <dmoitim@gmail.com>
2018-08-25 15:33:14 -0300

    Instalação do pacote MvvmLightLibs e validações iniciais dos campos email e senha.

--- lands/lands/ViewModels/LoginViewModel.cs
+++ lands/lands/ViewModels/LoginViewModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace lands.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using System;
     using System.Collections.Generic;
     using System.Text;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
     class LoginViewModel
     {
@@ -15,8 +17,51 @@
         #endregion
 
         #region Commands
-        public ICommand LoginCommand { get; set; }
+        public ICommand LoginCommand {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
         public ICommand RegistrarCommand { get; set; }
+
+        private async void Login() {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Erro",
+                    "Você deve informar o e-mail.",
+                    "OK");
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Senha))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Erro",
+                    "Você deve informar a senha.",
+                    "OK");
+
+                return;
+            }
+
+            if (this.Email != "dmoitim@gmail.com" || this.Senha != "1234")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Erro",
+                    "Usuário ou senha inváidos.",
+                    "OK");
+
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                "Sucesso",
+                "Seja bem vindo!",
+                "OK");
+        }
         #endregion
 
         #region Constructors
