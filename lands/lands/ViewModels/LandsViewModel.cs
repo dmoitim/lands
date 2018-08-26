41514477df4d26f8876ec35bae85323b6f24d427
Devair Luiz Moitim <dmoitim@gmail.com>
2018-08-26 13:24:53 -0300

    Implementação da comunicação para receber o JSON e checagem da conexão de internet.

--- lands/lands/ViewModels/LandsViewModel.cs
+++ lands/lands/ViewModels/LandsViewModel.cs
@@ -1,7 +1,70 @@
 ﻿namespace lands.ViewModels
 {
-    class LandsViewModel
-    {
+    using Models;
+    using Services;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
 
+    class LandsViewModel : BaseViewModel
+    {
+        #region Services
+        private ApiService apiService;
+        #endregion
+
+        #region Attributes
+        private ObservableCollection<Land> lands;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Land> Lands
+        {
+            get { return this.lands; }
+            set { SetValue(ref lands, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public LandsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadLands();
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadLands()
+        {
+            var connection = await this.apiService.CheckConnection();
+
+            if (!connection.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Erro",
+                    connection.Message,
+                    "OK");
+
+                await Application.Current.MainPage.Navigation.PopAsync();
+
+                return;
+            }
+
+            var response = await this.apiService.GetList<Land>(
+                "https://restcountries.eu",
+                "/rest",
+                "/v2/all");
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Erro",
+                    response.Message,
+                    "OK");
+
+                return;
+            }
+
+            var lista = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(lista);
+        } 
+        #endregion
     }
 }
